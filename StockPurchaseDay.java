4e876cd1cb05c2756c6ee776700bcdc95fd3a5e0
shivani26shinde <shindeshivani26@gmail.com>
2017-10-14 22:10:55 -0500

    Hackerrank Stock and Purchace question efficient solution

--- StockPurchaseDay.java
+++ StockPurchaseDay.java
@@ -1,5 +1,6 @@
-// Stock Purchase Day (Incomplete)
+// Stock Purchase Day 
 
+// Inefficient
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -41,6 +42,48 @@ public class StockPurchaseDay {
             }
             else if(map.get(val) > map.get(i)){
                 map.put(i, map.get(val));
+            }
+        }
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            int xi = in.nextInt();
+            int result = stockPurchaseDay(A, xi);
+            System.out.println(result);
+        }
+        in.close();
+    }
+}
+
+// Efficient
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int min  = Integer.MAX_VALUE;
+    static int stockPurchaseDay(int[] A, int xi) {
+        if(xi<min){
+            return -1;
+        }
+        for(int i=A.length-1;i>=0;i--){
+            if(A[i] <= xi){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] A = new int[n];
+        for(int A_i = 0; A_i < n; A_i++){
+            A[A_i] = in.nextInt();
+            if(min > A[A_i]){
+                min = A[A_i];
             }
         }
         int q = in.nextInt();
