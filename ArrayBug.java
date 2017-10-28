b06b8ad81b8e4a0c3fd49d4f144602cb0ab3c980
shivani26shinde <shindeshivani26@gmail.com>
2017-10-27 21:45:07 -0500

    Array Bug code

---
+++ ArrayBug.java
@@ -0,0 +1,12 @@
+# Array Bug
+# Rotate the array A by B positions
+
+public class Solution {
+	public ArrayList<Integer> rotateArray(ArrayList<Integer> A, int B) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (int i = 0; i < A.size(); i++) {
+			ret.add(A.get((i + B) % A.size()));
+		}
+		return ret;
+	}
+}
