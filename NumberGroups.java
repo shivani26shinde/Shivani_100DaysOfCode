493c7814c08ef1b70204407f87c2ef08080f9532
shivani26shinde <shindeshivani26@gmail.com>
2017-12-08 22:55:46 -0600

    Completed with optimized complexity

--- NumberGroups.java
+++ NumberGroups.java
@@ -1,4 +1,36 @@
-// Number Groups
+// Number Groups // Optimized
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long sumOfGroup(int k) {
+        long sum = 0;
+        long start = 1;
+        for(int i=1;i<k;i++){
+            start = start+2*i;
+        }
+        for(int i=1;i<=k;i++){
+            sum = sum+start;
+            start = start+2;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        long answer = sumOfGroup(k);
+        System.out.println(answer);
+        in.close();
+    }
+}
+
+// Number Groups // Brute Force
 
 import java.io.*;
 import java.util.*;
