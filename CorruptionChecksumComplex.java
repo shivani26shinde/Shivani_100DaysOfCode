d57ed80e9d8bc00a1cd2074dc0ca016038c90408
shivani26shinde <shindeshivani26@gmail.com>
2017-12-18 01:32:16 -0600

    In progress

---
+++ CorruptionChecksumComplex.java
@@ -0,0 +1,42 @@
+// Advent of Code 2017
+// Corrupption Checksum complex
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CorruptionChecksumComplex
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[][] array = new int[3][4];
+		Scanner sc = new Scanner(System.in);
+		for(int i=0;i<16;i++){
+			for(int j=0;j<16;j++){
+				array[i][j] = sc.nextInt();
+			}
+		}
+		int sum = 0;
+		for(int i=0;i<16;i++){
+			int[] arrayNew = new int[array[i].length];
+			for(int j=0;j<16;j++){
+				arrayNew[j] = array[i][j];
+			}
+			for(int j=0;j<arrayNew.length-1;j++){
+				for(int k=j+1;k<arrayNew.length;k++){
+					int val1 = 	arrayNew[j];
+					int val2 = 	arrayNew[k];
+					if(val1%val2 == 0){
+						sum = sum+(val1/val2);
+						break;
+					}
+					if(val2%val1 == 0){
+						sum = sum+(val2/val1);
+						break;
+					}
+				}
+			}
+		}
+		System.out.println("Sum is: "+sum);
+	}
+}
